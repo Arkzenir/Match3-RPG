29aeea8716673c538c79865030fe1fecb250dd3a
Alper Sari <alper.sari.work@gmail.com>
2021-07-16 13:01:15 +0300

    Slight change to project structure

--- Assets/Added Scripts/Singletons/Match3Master.cs
+++ Assets/Added Scripts/Singletons/Match3Master.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GemGridVisualDrag : MonoBehaviour
+public class Match3Master : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
